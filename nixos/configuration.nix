c84a47126c8b6ec6517dbd2892f1c3c8d835d15f
knollth <tom@knollth.xyz>
2025-03-31 16:53:16 +0200

    make neovim the default editor

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -149,6 +149,11 @@
     wl-clipboard
   ];
 
+  programs.neovim = {
+    enable = true;
+    defaultEditor = true;
+  };
+
   programs.steam = {
     enable = true;
     remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
