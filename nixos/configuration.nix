63614e49e4dedcebc323b143fd48b8dcea5efe9e
knollth <tom@knollth.xyz>
2025-04-02 12:28:59 +0200

    enable fwupd service

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -35,7 +35,13 @@
   };
   # setup mullvad vpn
   services.mullvad-vpn.enable = true;
-  
+
+  # ------ Framework config -------
+  services.fwupd.enable = true;
+
+
+
+
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
 
   # Configure network proxy if necessary
