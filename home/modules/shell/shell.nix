2abab16725cd63a9200a8a9ce31c9cd074c75c2d
knollth <tom@knollth.xyz>
2025-04-05 17:42:06 +0200

    adjusted envars

--- home/modules/shell/shell.nix
+++ home/modules/shell/shell.nix
@@ -1,9 +1,17 @@
 {config, pkgs,lib,...}:
 {
   home.shell.enableZshIntegration = true;
-  programs.zsh.enable = true;
+  programs.zsh = {
+    enable = true;
+    shellAliases = {
+      uni-push = "rclone sync -v $HOME/Documents/Studium drive:/Studium";
+      uni-pull = "rclone sync -v drive:/Studium $HOME/Documents/Studium";
+    };
+  };
+
   home.sessionVariables = {
-    EDITOR = "nvim";
+    EDITOR="nvim";
+    UNI="$HOME/Documents/Studium";
   };
 
   programs.oh-my-posh = {
