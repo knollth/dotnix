191e540b26349c892762bf7d5364c77efa94674b
knollth <tom@knollth.xyz>
2025-04-09 11:48:20 +0200

    add powertop to packages

--- home/modules/packages.nix
+++ home/modules/packages.nix
@@ -20,6 +20,7 @@
     onlyoffice-bin
     rclone
     typst
+    powertop
 
     # # It is sometimes useful to fine-tune packages, for example, by applying
     # # overrides. You can do that directly here, just don't forget the
