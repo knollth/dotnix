181d2ea7a06fb32ffbebd3ffb9eb5f7ee0f4df34
knollth <tom@knollth.xyz>
2025-04-11 12:56:00 +0200

    add reaper, sws, reapack packages to home

--- home/modules/packages.nix
+++ home/modules/packages.nix
@@ -24,6 +24,9 @@
     rclone
     typst
     powertop
+    reaper
+    reaper-sws-extension
+    reaper-reapack-extension
 
     # # It is sometimes useful to fine-tune packages, for example, by applying
     # # overrides. You can do that directly here, just don't forget the
